6df625f19c908aa0a5f56a5ca6b75da29961796a
JesseSutton8 <124111339+JesseSutton8@users.noreply.github.com>
2024-03-01 10:21:10 -0700

    completed encapsulation learning activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction Fraction1 = new Fraction();
+        Console.WriteLine(Fraction1.GetFractionString());
+        Console.WriteLine(Fraction1.GetDecimalValue());
+
+        Fraction Fraction2 = new Fraction(6);
+        Console.WriteLine(Fraction2.GetFractionString());
+        Console.WriteLine(Fraction2.GetDecimalValue());
+
+        Fraction Fraction3 = new Fraction(6, 7);
+        Console.WriteLine(Fraction3.GetFractionString());
+        Console.WriteLine(Fraction3.GetDecimalValue());
     }
 }
